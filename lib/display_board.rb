f05c44cabb8d7b20ec6091c0abd956bf3114e8ae
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-27 13:49:41 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  res = ""
+  cell = "   "
+  
+  row = cell + "|" + cell + "|" + cell
+  divider += "-----------"
+  
+  return row+"\n"
+  
+  
+  
+  
+  
+end
